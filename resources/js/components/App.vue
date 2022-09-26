000b0205938547557a3647fd6d614aabb51177b0
ManuelTano <manueltano98@gmail.com>
2022-09-26 17:35:26 +0200

    added vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="content">
+    <div class="title m-b-md">{{ title }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+        title: "Coming soon..",
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
