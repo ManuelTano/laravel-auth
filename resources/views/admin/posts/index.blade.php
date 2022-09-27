c975b801e63c61b0cd2b95eaef738aab4118a852
ManuelTano <manueltano98@gmail.com>
2022-09-27 16:00:08 +0200

    show

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,42 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-
-<div class="container">
-
-
-<table class="table table-striped table-dark border-1">
-    <thead>
-       
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Titolo</th>
-            <th scope="col">Slug</th>
-            <th scope="col">Creato il </th>
-            <th scope="col">Modificato il </th>
-            <th>Bottoni</th>
-        </tr>
-    </thead>
-    <tbody>
-        @forelse ($posts as $post)
-        <tr>
-            <th scope="row">{{ $post->id }}</th>
-            <td>{{ $post->title }}</td>
-            <td>{{ $post->slug }}</td>
-            <td>{{ $post->created_at }}</td>
-            <td>{{ $post->update_at }}</td>
-        </tr>
-        @empty
-        <tr>
-            <td class="colspan-6">
-                <h3 class="text*center">Nessun post</h3>
-            </td>
-        </tr>
-        @endforelse
-    </tbody>
-</table>
-
-</div>
+    <div class="container">
 
 
+        <table class="table table-striped table-dark border-1">
+            <thead>
+
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Titolo</th>
+                    <th scope="col">Slug</th>
+                    <th scope="col">Creato il </th>
+                    <th scope="col">Modificato il </th>
+                    <th>Bottoni</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($posts as $post)
+                    <tr>
+                        <th scope="row">{{ $post->id }}</th>
+                        <td>{{ $post->title }}</td>
+                        <td>{{ $post->slug }}</td>
+                        <td>{{ $post->created_at }}</td>
+                        <td>{{ $post->update_at }}</td>
+                        <td>
+                            <a href=" {{ route('admin.posts.show', $post) }}" class="btn btn-primary text-white">
+                                <i class="fa-solid fa-eye">Mostra</i></a>
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td class="colspan-6">
+                            <h3 class="text*center">Nessun post</h3>
+                        </td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+
+
+
+    </div>
 @endsection
