af3b115e1524fb66d5d57b615b9c6efdf3cd78d5
ManuelTano <manueltano98@gmail.com>
2022-09-27 15:05:31 +0200

    Created model, seeder and migration

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = 'manueltano';
+        $user->email = 'manueltano@gmail.com';
+        $user->password = bcrypt('password');
+        $user->save();
+    }
+}
